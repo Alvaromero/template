b7c90c676e387fc30c72837f99044190a191e388
Alvaromero <alvaro.jrp.1994@gmail.com>
2023-10-24 12:09:07 -0600

    demo

--- assets/js/chart-custom-sales.js
+++ assets/js/chart-custom-sales.js
@@ -30,6 +30,8 @@
   } );
 
   function setData(partnerId) {
+    jQuery("#load").fadeOut();
+    jQuery("#loading").delay().fadeOut("");
     /* FORECAST INFORMATION */
     const monthlySales = montlyData.filter(item => Number(item.brandId) == Number(partnerId)).reduce((total, item) => total + item.sales, 0);
     let options = {
@@ -539,6 +541,312 @@
     });
 
   }
+
+
+  const startYear = 1960,
+  endYear = 2018,
+  btn = document.getElementById('play-pause-button'),
+  input = document.getElementById('play-range'),
+  nbr = 20;
+
+let dataset, chart;
+
+
+/*
+ * Animate dataLabels functionality
+ */
+(function (H) {
+  const FLOAT = /^-?\d+\.?\d*$/;
+
+  // Add animated textSetter, just like fill/strokeSetters
+  H.Fx.prototype.textSetter = function () {
+    let startValue = this.start.replace(/ /g, ''),
+      endValue = this.end.replace(/ /g, ''),
+      currentValue = this.end.replace(/ /g, '');
+
+    if ((startValue || '').match(FLOAT)) {
+      startValue = parseInt(startValue, 10);
+      endValue = parseInt(endValue, 10);
+
+      // No support for float
+      currentValue = Highcharts.numberFormat(
+        Math.round(startValue + (endValue - startValue) * this.pos),
+        0
+      );
+    }
+
+    this.elem.endText = this.end;
+
+    this.elem.attr(this.prop, currentValue, null, true);
+  };
+
+  // Add textGetter, not supported at all at this moment:
+  H.SVGElement.prototype.textGetter = function () {
+    const ct = this.text.element.textContent || '';
+    return this.endText ? this.endText : ct.substring(0, ct.length / 2);
+  };
+
+  // Temporary change label.attr() with label.animate():
+  // In core it's simple change attr(...) => animate(...) for text prop
+  H.wrap(H.Series.prototype, 'drawDataLabels', function (proceed) {
+    const attr = H.SVGElement.prototype.attr,
+      chart = this.chart;
+
+    if (chart.sequenceTimer) {
+      this.points.forEach(point =>
+        (point.dataLabels || []).forEach(
+          label =>
+            (label.attr = function (hash) {
+              if (
+                hash &&
+                hash.text !== undefined &&
+                chart.isResizing === 0
+              ) {
+                const text = hash.text;
+
+                delete hash.text;
+
+                return this
+                  .attr(hash)
+                  .animate({ text });
+              }
+              return attr.apply(this, arguments);
+
+            })
+        )
+      );
+    }
+
+    const ret = proceed.apply(
+      this,
+      Array.prototype.slice.call(arguments, 1)
+    );
+
+    this.points.forEach(p =>
+      (p.dataLabels || []).forEach(d => (d.attr = attr))
+    );
+
+    return ret;
+  });
+}(Highcharts));
+
+
+function getData(year) {
+  const output = Object.entries(dataset)
+    .map(country => {
+      const [countryName, countryData] = country;
+      return [countryName, isNaN(Number(countryData[year])) ? 0 : Number(countryData[year])];
+    })
+    .sort((a, b) => b[1] - a[1]);
+  return [output[0], output.slice(1, nbr)];
+}
+
+function getMonthName(monthNumber) {
+  const months = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+  ];
+
+  if (monthNumber >= 1 && monthNumber <= 12) {
+      return months[monthNumber - 1];
+  } else {
+      return "Invalid month number";
+  }
+}
+
+function getSubtitle() {
+  const population = (getData(input.value)[0][1]).toFixed(2);
+  const value = getMonthName(input.value);
+  return `<span style="font-size: 80px">${value}</span>
+    <br>
+    <span style="font-size: 22px">
+      Total: <b>: ${new Intl.NumberFormat('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 }).format(population)}</b> dollars
+    </span>`;
+}
+
+(async () => {
+
+  /*dataset = await fetch(
+    'https://demo-live-data.highcharts.com/population.json'
+  ).then(response => response.json());*/
+
+  dataset = {};
+
+    window.productsData.forEach(item => {
+        const { asin, month, sales, month_number } = item;
+
+        if (!dataset[asin]) {
+          dataset[asin] = {};
+        }
+
+        dataset[asin][month_number] = parseFloat(sales);
+    });
+
+
+  chart = Highcharts.chart('container', {
+    chart: {
+      animation: {
+        duration: 500
+      },
+      marginRight: 50
+    },
+    title: {
+      text: 'Sales of main products in 2023',
+      align: 'left'
+    },
+    subtitle: {
+      useHTML: true,
+      text: getSubtitle(),
+      floating: true,
+      align: 'right',
+      verticalAlign: 'middle',
+      y: -80,
+      x: -100
+    },
+
+    legend: {
+      enabled: false
+    },
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      opposite: true,
+      tickPixelInterval: 150,
+      title: {
+        text: null
+      }
+    },
+    tooltip: {
+      formatter: function() {
+        return `${this.key}<br>${getMonthName(this.series.name)}: <b>${new Intl.NumberFormat('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 }).format(this.y)}</b>`
+      }
+    },
+    plotOptions: {
+      series: {
+        animation: false,
+        groupPadding: 0,
+        pointPadding: 0.1,
+        borderWidth: 0,
+        colorByPoint: true,
+        dataSorting: {
+          enabled: true,
+          matchByName: true
+        },
+        type: 'bar',
+        dataLabels: {
+          enabled: true
+        }
+      }
+    },
+    series: [
+      {
+        type: 'bar',
+        name: startYear,
+        data: getData(startYear)[1]
+      }
+    ],
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 550
+        },
+        chartOptions: {
+          xAxis: {
+            visible: false
+          },
+          subtitle: {
+            x: 0
+          },
+          plotOptions: {
+            series: {
+              dataLabels: [{
+                enabled: true,
+                y: 8
+              }, {
+                enabled: true,
+                format: '{point.name}',
+                y: -8,
+                style: {
+                  fontWeight: 'normal',
+                  opacity: 0.7
+                }
+              }]
+            }
+          }
+        }
+      }]
+    }
+  });
+})();
+
+/*
+ * Pause the timeline, either when the range is ended, or when clicking the pause button.
+ * Pausing stops the timer and resets the button to play mode.
+ */
+function pause(button) {
+  button.title = 'play';
+  button.className = 'fa fa-play';
+  clearTimeout(chart.sequenceTimer);
+  chart.sequenceTimer = undefined;
+}
+
+/*
+ * Update the chart. This happens either on updating (moving) the range input,
+ * or from a timer when the timeline is playing.
+ */
+function update(increment) {
+  if (increment) {
+    input.value = parseInt(input.value, 10) + increment;
+  }
+  if (input.value >= endYear) {
+    // Auto-pause
+    pause(btn);
+  }
+
+  chart.update(
+    {
+      subtitle: {
+        text: getSubtitle()
+      }
+    },
+    false,
+    false,
+    false
+  );
+
+  chart.series[0].update({
+    name: input.value,
+    data: getData(input.value)[1]
+  });
+}
+
+/*
+ * Play the timeline.
+ */
+function play(button) {
+  button.title = 'pause';
+  button.className = 'fa fa-pause';
+  chart.sequenceTimer = setInterval(function () {
+    update(1);
+  }, 500);
+}
+
+btn.addEventListener('click', function () {
+  if (chart.sequenceTimer) {
+    pause(this);
+  } else {
+    play(this);
+  }
+});
+/*
+ * Trigger the update on the range bar click.
+ */
+input.addEventListener('click', function () {
+  update();
+});
+
+update();
 
 
 })(jQuery);
